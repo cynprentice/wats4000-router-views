c23b979cb08eb7292eb144efb208931f83b09ee9
cynprentice <cprentice@seattleu.edu>
2020-05-03 12:47:51 -0700

    added Q6 to survey

--- src/views/Survey.vue
+++ src/views/Survey.vue
@@ -4,31 +4,38 @@
       <p>Please complete the new member survey.</p>
       <form v-on:submit.prevent="validateForm">
         <p class="error-message" v-show="showError"> {{ errorMessage }} </p>
-        <p><label for="q1">Q1: How long have you been building websites?<br><input type="text" id="q1" v-model="q1"  tabindex="0"></label></p>
-        <p>Q2: What languages interest you the most?<br>
+        <p><label for="q1">Q1: How long have you been building websites?*<br><input type="text" id="q1" v-model="q1"  tabindex="0"></label></p>
+        <p>Q2: What languages interest you the most?*<br>
         <label v-for="language in languageOptions" :key="language.value">
           <input type="checkbox" v-model="q2" v-bind:value="language.value">
              {{ language.text}}
         </label>
         </p>
 
-        <p>Q3: What other topics interest you?<br>
-          <label v-for="topic in topicOptions" :key="topic.value">
+        <p>Q3: What other topics interest you?*<br>
+         <label v-for="topic in topicOptions" :key="topic.value">
           <input type="checkbox" v-model="q3" v-bind:value="topic.value">
           {{ topic.text}}
-        </label>        </p>
+        </label>
+        </p>
         <p>
-          <label for="q4">Q4: What kinds of websites would you like to build someday?<br>
+          <label for="q4">Q4: What kinds of websites would you like to build someday?*<br>
             <textarea cols="70" rows="8" id="q4" v-model="q4" placeholder="Type your response here."></textarea>
           </label>
         </p>
         <p>
-          <label for="q5">Q5: Spaces or Tabs?
+          <label for="q5">Q5: Spaces or Tabs?*
             <select id="q5" v-model="q5">
               <option value="">Select your preference.</option>
               <option value="spaces">Spaces</option>
               <option value="tabs">Tabs</option>
             </select>
+          </label>
+        </p>
+        <p>Q6: What are your ideal work hours?(optional)<br>
+        <label v-for="location in workLocations" :key="location.value">
+          <input type="radio" v-model="q6" v-bind:value="location.value">
+              {{location.text}} <br>
           </label>
         </p>
         <p><input type="submit" value="Submit"></p>
@@ -47,6 +54,7 @@ export default {
       q3: [],
       q4: '',
       q5: '',
+      q6: '',
       languageOptions: [
         {
           text: 'JavaScript',
@@ -89,6 +97,24 @@ export default {
         {
           text: 'Multimedia',
           value: 'media'
+        }
+      ],
+      workLocations: [
+        {
+          text: 'Company Office',
+          value: 'company'
+        },
+        {
+          text: 'Co-working Space',
+          value: 'coworking'
+        },
+        {
+          text: 'Home',
+          value: 'home'
+        },
+        {
+          text: 'Variable Remote Locations',
+          value: 'remote'
         }
       ],
       errorMessage: ''
